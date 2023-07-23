a2d06cdffd744f412eb68a7f1ebc49e03b1e360b
strangekids7822 <keji_zhaoqiang2006@163.com>
2023-07-23 22:06:14 +0800

    Create directory of  and connect to SQLite database.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose(); // import sqlite3 package
 const app = express();
 const port = process.env.PORT || 3001;
+
+// Connect to SQLite database
+let db = new sqlite3.Database('./db/words.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the words database.');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,4 +17,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
-})
+});
