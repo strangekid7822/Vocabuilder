9693d99c03575bb649f236774d0420a9c466eaa3
strangekids7822 <keji_zhaoqiang2006@163.com>
2023-07-23 16:48:53 +0800

    Create WordForm.css for WorForm and import it.

--- src/components/WordForm.js
+++ src/components/WordForm.js
@@ -1,8 +1,7 @@
-// src/components/WordForm.js
-
 import React, { useContext, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { WordContext } from '../WordContext';
+import './WordForm.css';
 
 const WordForm = () => {
     const [words, setWords] = useContext(WordContext);
