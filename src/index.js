758cc7d109407b7a9d32eb9883b14ccd2bd116f8
strangekids7822 <keji_zhaoqiang2006@163.com>
2023-07-23 14:50:43 +0800

    Modified the imported css files' order, now the App.cs > bootstrap.css > index.css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
 import { WordProvider } from './WordContext';
 
 ReactDOM.render(
