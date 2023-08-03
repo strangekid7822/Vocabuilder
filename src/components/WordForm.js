5df8fbda38171129c5c98cc02132750ff7090cef
赵强 <zhaoqiang@zhaoqiangs-MacBook-Air-2.local>
2023-08-03 12:08:26 +0800

    In the WordForm Component, send a POST request to  when a word is submitted.

--- src/components/WordForm.js
+++ src/components/WordForm.js
@@ -1,16 +1,34 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react'; // Removed useContext and WordContext as they are not needed now
 import { Form, Button } from 'react-bootstrap';
-import { WordContext } from '../WordContext';
 import './WordForm.css';
 
 const WordForm = () => {
-    const [words, setWords] = useContext(WordContext);
     const [newWord, setNewWord] = useState('');
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => { // Made the function async
         event.preventDefault();
-        setWords([...words, newWord]);
-        setNewWord('');
+
+        // Create an object to hold the word data
+        const wordData = {
+            word: newWord, // You can add other fields here as needed (word_class, meaning, etc.)
+        };
+
+        // Send a POST request to the server with the word data
+        const response = await fetch('/words', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(wordData),
+        });
+
+        // Handle the response (e.g., show a success message, clear the form, etc.)
+        if (response.ok) {
+            setNewWord(''); // Clear the form input
+            alert('Word added successfully!'); // Show a success message (you can replace this with a more user-friendly UI)
+        } else {
+            alert('Failed to add word. Please try again.'); // Show an error message
+        }
     };
 
     return (
